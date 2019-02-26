c0e2e710ae6a4379861e016096f0b82668b7b5c8
KaiLung Sun <kaisun@elevenbuckets.com>
2019-02-26 13:53:13 -0700

    add sanity files

---
+++ dapps/Optract/Conditions/Optract/Optract/Sanity.js
@@ -0,0 +1,51 @@
+module.exports = 
+{
+	Optract_fillInEth_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+	Optract_claimOptract_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+	Optract_claimOptract_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+	Optract_putOnStock_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+	Optract_currentOwnerExercise_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+	Optract_ethSellerWithdraw_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+	Optract_queryOptionPrice_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+        Optract_queryOrderPrice_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+        Optract_queryOrderSize_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+        Optract_queryOrderSize_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+        Optract_queryOnStock_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+        Optract_isFilled_sanity(addr, jobObj) 
+	{
+		return true;
+	}
+}
---
+++ dapps/Optract/Conditions/Optract/OptractRegistry/Sanity.js
@@ -0,0 +1,27 @@
+module.exports = 
+{
+	OptractRegistry_createOptract_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+	OptractRegistry_queryInitPrice_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+	OptractRegistry_queryInitPrice_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+	OptractRegistry_isExpired_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+	OptractRegistry_getExpireTime_sanity(addr, jobObj) 
+	{
+		return true;
+	},
+	OptractRegistry_activeOptracts_sanity(addr, jobObj) 
+	{
+		return true;
+	}
+}

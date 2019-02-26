edd8dabbbb915050939c3dcb37315c39539601c5
jasonlin <jasonlin@elevenbuckets.com>
2019-02-26 13:27:47 -0700

    add throw for debug

--- migrations/2_deploy_DAI_Optract_Registry.js
+++ migrations/2_deploy_DAI_Optract_Registry.js
@@ -1,8 +1,8 @@
-var Registry = artifacts.require("./Registry.sol");
+var Registry = artifacts.require("./OptractRegistry.sol");
 var Optract  = artifacts.require("./Optract.sol");
 var ERC20 = artifacts.require("./ERC20.sol");
 var StandardToken = artifacts.require("./StandardToken.sol");
-var RNT = artifacts.require("./DAI.sol");
+var DAI = artifacts.require("./DAI.sol");
 var SafeMath = artifacts.require("./SafeMath.sol");
 
 module.exports = function(deployer) {
@@ -12,13 +12,17 @@ module.exports = function(deployer) {
   deployer.deploy(DAI).then((iDAI) => {
 	let DAIAddr = iDAI.address;
   	deployer.link(SafeMath, Optract);
-	deployer.deploy(Optract, "0", "0", "0x0", "0x0", "0x0", DAIAddr).then(() => {
+	return deployer.deploy(Optract, "0", "0", "0x0000000000000000000000000000000000000000"
+					 , "0x0000000000000000000000000000000000000000"
+					 , "0x0000000000000000000000000000000000000000", DAIAddr).then(() => 
+	{
   		deployer.link(Optract, Registry);
-		deployer.deploy(Registry, DAIAddr).then((iReg) => {
+		return deployer.deploy(Registry, DAIAddr).then((iReg) => {
 			console.log(`ETH-DAI Registry Address: ${iReg.address}`);
 			console.log(`Done!`);
 		})
 	})
-	.catch((err) => { console.trace(err) });
+	.catch((err) => { console.trace(err); throw err; });
   })
-  .catch((err) => { console.trace(err) });
+  .catch((err) => { console.trace(err); throw err; });
+}

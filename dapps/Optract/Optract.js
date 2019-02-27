933f03d199fc9e390f0e4acbbd1c0caa26a234fe
jasonlin <jasonlin@elevenbuckets.com>
2019-02-27 16:28:17 -0700

    adding local solution to new batch functions

--- dapps/Optract/Optract.js
+++ dapps/Optract/Optract.js
@@ -9,32 +9,47 @@ const biapi = require('bladeiron_api');
 // 11BE BladeIron Client API
 const BladeIronClient = require('bladeiron_api');
 
+// Functional
+const comprise = (...fns) => args => { return fns.reduce((r,f) => {return f(r)}, args); };
+
 class Optract extends BladeIronClient {
 	constructor(rpcport, rpchost, options)
         {
 		super(rpcport, rpchost, options);
 	        this.ctrName = 'OptractRegistry';
 
+		this.Tk = (ctrName) => (callName) => (...args) => (amount = null) => (jobList = []) => 
+		{
+			return [ ...jobList, this.getTkObj(ctrName)(callName)(...args)(amount) ]
+		}
+
+		this.manualGasBatch = (gasAmount) => (...fns) =>
+		{
+			this.gasAmount = gasAmount;
+			let p = comprise(...fns)();
+			return Promise.all(p).then((jobList) => { return this.processJobs(jobList) })
+		}
+
                 this.createOptract = (ethAmount, totalPrice, period) => {
-                        let p = [
-                                typeof(this.initialPayment) === 'undefined' ? this.queryInitPrice() : this.initialPayment,
-                                this.getTkObj('DAI')('approve')(this.ctrAddrBook['OptractRegistry'], this.initialPayment)(),
-                                this.getTkObj(this.ctrName)('createOptract')(ethAmount, totalPrice, period)()
-                        ];
-                        return Promise.all(p).then((plist) => {
-                                return this.processJobs(plist.slice(1)).then((QID) => {
+			return this.queryInitPrice().then((tokenAmount) => {
+				console.log(`DEBUG: initPrice = ${tokenAmount}`);
+                       		return this.manualGasBatch(2000000)(
+					this.Tk('DAI')('approve')(this.ctrAddrBook['OptractRegistry'], tokenAmount)(),
+                                        this.Tk(this.ctrName)('createOptract')(ethAmount, totalPrice, period)()
+				).then((QID) => {
+					console.log(`DEBUG: gasAmount after processJobs: ${this.gasAmount}`);
 					return this.getReceipts(QID).then((QIDlist) => {
-                                                if (QIDlist[0].status !== '0x1') throw "failed to approve";
-                                                if (QIDlist[1].status !== '0x1') throw "failed to create optract";
-                                                return {'QIDlist': QIDlist, 'txHash': QIDlist[1].transactionHash};
-                                        });
-                                })
-                        })
+                            		        if (QIDlist[0].status !== '0x1') throw "failed to approve";
+                                              	if (QIDlist[1].status !== '0x1') throw "failed to create optract";
+                            		        return {'QIDlist': QIDlist, 'txHash': QIDlist[1].transactionHash};
+                                      	});
+                              	});
+			});
                 }
 
                 this.queryInitPrice = () => {
                         return this.call(this.ctrName)('queryInitPrice')().then((rc) => {
-                                this.initialPayment = rc
+				return rc;
                         })
                 }
 

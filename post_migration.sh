82dd7e43618e478278491e5e311b70bf62e044d5
KaiLung Sun <kaisun@elevenbuckets.com>
2019-03-04 15:44:48 -0700

    update config and add BlockRegistry artifact

--- post_migration.sh
+++ post_migration.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-cp build/contracts/Optract.json build/contracts/OptractRegistry.json build/contracts/DAI.json build/contracts/MemberShip.json ./dapps/Optract/ABI && \
+cp build/contracts/Optract.json build/contracts/OptractRegistry.json build/contracts/DAI.json build/contracts/MemberShip.json build/contracts/BlockRegistry.json ./dapps/Optract/ABI && \
      cd dapps/Optract/ABI && \
      jq -r '.abi' Optract.json > Optract.abi && \
      jq -r '.abi' OptractRegistry.json > OptractRegistry.abi && \
      jq -r '.abi' DAI.json > DAI.abi && \
+     jq -r '.abi' BlockRegistry.json > BlockRegistry.abi && \
      jq -r '.abi' MemberShip.json > MemberShip.abi
 
 echo $?
